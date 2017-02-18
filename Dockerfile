38ee2ea8a56a9c0f6546099a2eb5fc8f30267bef
John Boiles <johnaboiles@gmail.com>
2017-02-17 23:36:55 -0800

    GPS support

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM mpercival/resin-rtl-sdr
 MAINTAINER John Boiles
 
 RUN apt-get update \
-&& apt-get install libasound2-dev
+&& apt-get install libasound2-dev libgps-dev
 
 RUN cd ~ \
 && git clone https://www.github.com/wb2osz/direwolf \
--- run.sh
+++ run.sh
@@ -30,20 +30,44 @@ fi
 echo "IGSERVER: $IGSERVER"
 sed -i -e "s/{{IGSERVER}}/${IGSERVER}/g" $CONF
 
-# TODO: Also support dynamic PBEACONs via gpsd
+fill_or_strip_comment () {
+	if [ -n "$COMMENT" ]; then
+		echo "PBEACON COMMENT: $COMMENT"
+		sed -i -e "s/{{COMMENT}}/\"${COMMENT}\"/g" $CONF
+	else
+		# Strip comment from PBEACON line
+		sed -i -e "s/comment={{COMMENT}}//g" $CONF
+	fi
+}
+
+# Users of this script can either provide LATITUDE & LONGITUDE, NMEA_GPS, or
+# GPSD_HOST in order to send position beacons
 if [[ -n "$LATITUDE" ]] && [[ -n "$LONGITUDE" ]]; then
 	echo "PBEACON LATITUDE: $LATITUDE"
 	echo "PBEACON LONGITUDE: $LONGITUDE"
 	sed -i -e "s/{{LATITUDE}}/${LATITUDE}/g" $CONF
 	sed -i -e "s/{{LONGITUDE}}/${LONGITUDE}/g" $CONF
 	sed -i -e "s/#PBEACON/PBEACON/g" $CONF
-	if [ -n "$COMMENT" ]; then
-		sed -i -e "s/{{COMMENT}}/\"${COMMENT}\"/g" $CONF
-		echo "PBEACON COMMENT: $COMMENT"
+	fill_or_strip_comment
+elif [ -n "$NMEA_GPS" ]; then
+	echo "NMEA_GPS: $NMEA_GPS"
+	# Use % here instead of / since device descriptors typically have / in them
+	sed -i -e "s%{{NMEA_GPS}}%${NMEA_GPS}%g" $CONF
+	sed -i -e "s/#GPSNMEA/GPSNMEA/g" $CONF
+	sed -i -e "s/#TBEACON/TBEACON/g" $CONF
+	fill_or_strip_comment
+elif [ -n "$GPSD_HOST" ]; then
+	echo "GPSD_HOST: $GPSD_HOST"
+	sed -i -e "s/{{GPSD_HOST}}/${GPSD_HOST}/g" $CONF
+	sed -i -e "s/#GPSD/GPSD/g" $CONF
+	sed -i -e "s/#TBEACON/TBEACON/g" $CONF
+	if [ -n "$GPSD_PORT" ]; then
+		sed -i -e "s/{{GPSD_PORT}}/${GPSD_PORT}/g" $CONF
 	else
-		# Strip comment from PBEACON line
-		sed -i -e "s/comment={{COMMENT}}//g" $CONF
+		# Fall back to default gpsd port 2947
+		sed -i -e "s/{{GPSD_PORT}}/2947/g" $CONF
 	fi
+	fill_or_strip_comment
 fi
 
 if [ -n "$IGFILTER" ]; then
